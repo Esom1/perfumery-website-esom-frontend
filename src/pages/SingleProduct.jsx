211d9c9de2e3ad80fa8cd4f347a1ff1121a75eb3
christy <christinaugoala@gmail.com>
2023-07-05 02:34:52 +0100

    modified singleproduct

--- src/pages/SingleProduct.jsx
+++ src/pages/SingleProduct.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import CartContext from '../hooks/CartContext';
 import '../style/SingleProduct.css'
 import UseFetch from '../hooks/UseFetch';
 import {useParams, Link } from 'react-router-dom'
@@ -10,6 +11,7 @@ import tag from '../assets/tag.png'
 
 
 const SingleProduct = () => {
+  const {handleAddToCart} = useContext(CartContext)  
   const {id} = useParams()
   const { data,loading } = UseFetch(`https://perfumerysite.onrender.com/perfumery/specificpost/${id}`)
   const {title,image,description,price,category} = data
@@ -17,39 +19,45 @@ const SingleProduct = () => {
   return (
     <div className='container'>
       <h3>{loading}</h3>
-      <div className='row'>
-        <div>
-          <img src={image} alt="" />
+      <div className='row mt-5 gap-2'>
+        <div className='try'>
+        <div className='border border-2 rounded shadow-5 '>
+          <img className='image-fluid w-100 p-3' src={image} alt="" />
         </div>
-       <div>
+        <div className='border border-2 shadow-5 rounded '>
+       <div className='p-3'>
        <h4>Product Name</h4>
-        <p>{title}</p>
-        <p>{category}</p>
+        <h5 className='del'>{title}</h5>
+        <h4>Description</h4>
           <p>{description}</p>
-          <p>{price}</p>
-        <button className='btn shop'>Shop Now</button>
-        <button className='btn add'>Add to Cart</button>
+          <h4 className='span'>#{price}</h4>
+       <div className='d-flex gap-3'>
+       <button onClick={()=>{handleAddToCart(datum)}}  className='btn shop'>Shop Now</button>
+        <button onClick={()=>{handleAddToCart(datum)}}  className='btn add'>Add to Cart</button>
        </div>
-       <div>
-       <div className='d-flex gap-5 pt-5'>
+       </div>
+       </div>
+       <div className='straighten border border-2 shadow-5 rounded '>
+       <div className=' p-3'>
        <div className='d-flex gap-2'>
-        <img className='bus' src={bus} alt="" />
+        <img className='bus2' src={bus} alt="" />
         <p className='mt-2'>Nationwide Delivery <br /> Anywhere Anytime</p>
       </div>
       <div className='d-flex gap-2'>
-        <img className='bus' src={deliver} alt="" />
+        <img className='bus2' src={deliver} alt="" />
         <p className='mt-2'>Same Day Delivery <br />In Lagos Only</p>
       </div>
       <div className='d-flex gap-2'>
-        <img className='bus' src={pay} alt="" />
+        <img className='bus2' src={pay} alt="" />
         <p className='mt-2'>Pay On Delivery <br />In Lagos Only</p>
       </div>
       <div className='d-flex gap-2'>
-        <img className='bus' src={tag} alt="" />
+        <img className='bus2' src={tag} alt="" />
         <p className='mt-2'>Lowest Price <br />Best Deal</p>
         </div>
        </div>
        </div>
+      </div>
       </div>
     </div>
   )
